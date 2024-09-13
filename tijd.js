81023fe35100d5c6021a781d719e58042e1f29d5
Cats-R-Cool <38488@ma-web.nl>
2024-09-13 15:51:09 +0200

    Add files via upload

---
+++ tijd.js
@@ -0,0 +1,8 @@
+const date = new Date(1985, 6, 12);
+console.log("1. " + date);
+const date2 = new Date(1955, 8, 23);
+console.log("2. " + date2);
+const date3 = new Date(2015, 7, 31);
+console.log("3. " + date3);
+const date4 = new Date(1885, 0, 4);
+console.log("4. " + date4);
