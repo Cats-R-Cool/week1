aa58efc4de9d66089a44df5cd2323f8b1005faa4
Cats-R-Cool <38488@ma-web.nl>
2024-09-13 15:30:27 +0200

    Add files via upload

---
+++ playerstatistics.js
@@ -0,0 +1,10 @@
+let name = "Felix Rademaker"
+console.log ("Name: " + name);
+let HP = "250"
+console.log ("Health points: " + HP);
+let power = "Procrastination"
+console.log ("Special power: " + power);
+let weakness = "Bad music"
+console.log ("Weakness: " + weakness);
+let speed = "300"
+console.log ("Speed: " + speed);
